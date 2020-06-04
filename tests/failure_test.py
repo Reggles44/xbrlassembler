a00ceb9a479d13dbfef0ef5226631650ce2a4709
Reginald B <reginaldbeakes@gmail.com>
2020-06-04 15:54:33 -0400

    0.7.1
    Updating tests for coverage

--- tests/failure_test.py
+++ tests/failure_test.py
@@ -1,6 +1,6 @@
 import re
 
-from xbrlassembler import XBRLElement, XBRLAssembler
+from xbrlassembler import XBRLElement, XBRLAssembler, DateParser
 
 
 def makes_exception(func, *args, **kwargs):
@@ -20,7 +20,7 @@ def test_failure():
 
     assert makes_exception(XBRLAssembler, "info", "schema", "data", "label", "ref")
     assert makes_exception(XBRLAssembler.from_sec_index, "google index please")
-
+    assert makes_exception(XBRLAssembler.from_dir, 'Yolo directory')
     assert makes_exception(XBRLAssembler.from_sec_index, "https://www.sec.gov/Archives/edgar/data/1652044/0001652044-20-00002-index.htm")
 
     google_index = "https://www.sec.gov/Archives/edgar/data/1652044/0001652044-20-000021-index.htm"
@@ -28,3 +28,5 @@ def test_failure():
     assert makes_exception(google_assembler.get, "no")
     assert makes_exception(google_assembler.get, 'Not going to find a thing')
     assert makes_exception(google_assembler.get, re.compile('Not going to find a thing'))
+
+    assert DateParser.parse('Yolo') is None
