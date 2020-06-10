9e346456a792aca47fc99585286fb1f947cafc14
Reginald B <reginaldbeakes@gmail.com>
2020-06-10 10:29:54 -0400

    0.8.12
    Added check for no columns found with corresponding XBRLError

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,7 +1,10 @@
+import pytest
+
 from tests import assembler_test
-from xbrlassembler import XBRLAssembler
+from xbrlassembler import XBRLAssembler, XBRLError
 
 
+@pytest.mark.xfail(raises=XBRLError)
 def test_assembler():
     url = "https://www.sec.gov/Archives/edgar/data/712537/0000712537-20-000012-index.htm"
 
