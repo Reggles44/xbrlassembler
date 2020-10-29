ed10529dc1221351ea3c3c61fe99454e83038f3c
Reginald <reginaldbeakes@gmail.com>
2020-10-28 23:47:46 -0400

    0.11.2
    Updating CI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="XBRLAssembler",
-    version="0.11.1",
+    version="0.11.2",
     author="Reggles",
     author_email="reginaldbeakes@gmail.com",
     description="An assembler for XBRL Documents into pandas",
