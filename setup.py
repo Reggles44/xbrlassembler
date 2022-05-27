526799c3fc2155afc438237a85c19c1fe655d17f
Reginald <reginaldbeakes@gmail.com>
2022-05-26 22:06:48 -0400

    Updating setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
 setup(
-    name="XBRLAssembler",
-    version="0.13.1",
+    name="xbrlassembler",
+    version="0.14.0",
     author="Reggles",
     author_email="reginaldbeakes@gmail.com",
-    description="An assembler for XBRL Documents into pandas",
+    description="An assembler of XBRL documents",
     url="https://gitlab.com/Reggles44/xbrlassembler",
     include_package_data=True,
     packages=['xbrlassembler'],
